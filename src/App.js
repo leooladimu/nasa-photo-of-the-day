bf9059220ea89c7ded4e1060f2f28fa2f2022bac
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-08-11 22:47:56 -0400

    Leo-O

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BASE_URL, API_KEY } from './constants/index';
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+
+const [apod, setApod] = useState([])
+// const [date, setDate] = useState([])
+const [err, setError] = useState(null)
+
+useEffect(() => {
+  axios.get(`https://api.nasa.gov/planetary/apod/?api_key=${API_KEY}`)
+  .then(res => {
+    console.log(res.data);
+    setApod(res)
+    console.log(res.data);
+    // console.log(apod);
+  })
+  .catch(err => {
+    console.error(err);
+    setError('Sorry')
+  })
+}, [])
+
+
   return (
     <div className="App">
       <p>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod';
+export const API_KEY = 'E1fdsonrxSM0KXX7gTtsdSXrdd7zvyzDYeyyr9HH';
